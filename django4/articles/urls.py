8b2b3c8587a0dce6aa1aaa55bfcae229719d42fc
Ivan Bowen <ivanovicbow850@gmail.com>
2022-03-16 12:07:50 +0300

    added loginrequiredmixixn for authetication

--- django4/articles/urls.py
+++ django4/articles/urls.py
@@ -4,5 +4,8 @@ from . import views
 app_name = 'articles'
 urlpatterns = [
     path('', views.HomeView.as_view(), name='all'),
-    path('article/create/', views.ArticleCreate.as_view(), name='article_create')
+    path('create/', views.ArticleCreate.as_view(), name='article_create'),
+    path('update/<int:pk>/', views.ArticleUpdate.as_view(), name='article_update'),
+    path('delete/<int:pk>/', views.ArticleDelete.as_view(), name='article_delete'),
+    path('<int:pk>/', views.ArticleDetail.as_view(), name='article_detail'),
 ]
--- django4/articles/views.py
+++ django4/articles/views.py
@@ -1,18 +1,22 @@
 import random
 
+from django.contrib.auth.mixins import LoginRequiredMixin
 from django.shortcuts import render
 from django.urls import reverse_lazy
 from django.views import View
+from django.views.generic import DetailView
+
 from .models import Article
 from django.http import HttpResponse
 from django.views.generic.edit import CreateView, UpdateView, DeleteView
 
+
 # Create your views here.
 
-class HomeView(View):
+class HomeView(LoginRequiredMixin, View):
 
     def get(self, request):
-
+        # print(request.user)
         article_obj = Article.objects.get(id=1)
         count = Article.objects.all().count()
         articles = Article.objects.all()
@@ -20,8 +24,25 @@ class HomeView(View):
 
         return render(request, 'articles/article_list.html', ctx)
 
-class ArticleCreate(CreateView):
 
+class ArticleCreate(LoginRequiredMixin, CreateView):
     model = Article
     fields = "__all__"
     success_url = reverse_lazy('articles:all')
+
+
+class ArticleUpdate(LoginRequiredMixin, UpdateView):
+    model = Article
+    fields = "__all__"
+    success_url = reverse_lazy('articles:all')
+
+
+class ArticleDelete(LoginRequiredMixin, DeleteView):
+    model = Article
+    fields = "__all__"
+    success_url = reverse_lazy('articles:all')
+
+
+class ArticleDetail(LoginRequiredMixin, DetailView):
+    model = Article
+    template_name = 'articles/article_detail.html'
